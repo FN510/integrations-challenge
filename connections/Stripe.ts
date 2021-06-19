6c8f8efaabab370fcff4e4c1cf770c673f2680c6
FN510 <f.ngemoh@warwick.ac.uk>
2021-06-19 15:14:02 +0100

    add ParsedAuthorizationResponse Promise

--- connections/Stripe.ts
+++ connections/Stripe.ts
@@ -39,9 +39,10 @@ const StripeConnection: ProcessorConnection<APIKeyCredentials, CardDetails> = {
     let createPaymentIntentParams = new URLSearchParams();
     createPaymentIntentParams.append('amount', request.amount);
     createPaymentIntentParams.append('currency', request.currencyCode);
-
-
+    // seperate auth and capture
+    createPaymentIntentParams.append('capture_method', "manual"); 
     let key = this.configuration.apiKey;
+    return new Promise<ParsedAuthorizationResponse>((resolve, reject) => {
     HttpClient.request(url,
       {
         method: "post",
@@ -49,36 +50,38 @@ const StripeConnection: ProcessorConnection<APIKeyCredentials, CardDetails> = {
         body: createPaymentIntentParams
       }
     )
-      .then(function (res) {
+      .then((res) => {
         var pi_id = JSON.parse(res.responseText).id;
-        console.log("pi_id: " + pi_id)
+        //console.log("pi_id: " + pi_id);
 
         // post request to create a PaymentMethod (card details)
         let urlCreatePaymentMethod = "https://api.stripe.com/v1/payment_methods";
         let cardDetails = request.paymentMethod;
         let cardBody = new URLSearchParams();
-        cardBody.append('type', 'card')
-        cardBody.append('card[number]', cardDetails.cardNumber)
-        cardBody.append('card[exp_month]', cardDetails.expiryMonth)
-        cardBody.append('card[exp_year]', cardDetails.expiryYear)
-        cardBody.append('card[cvc]', cardDetails.cvv)
+        cardBody.append('type', 'card');
+        cardBody.append('billing_details[name]', cardDetails.cardholderName);
+        cardBody.append('card[number]', cardDetails.cardNumber);
+        cardBody.append('card[exp_month]', cardDetails.expiryMonth);
+        cardBody.append('card[exp_year]', cardDetails.expiryYear);
+        cardBody.append('card[cvc]', cardDetails.cvv);
+      
 
 
-        console.log(cardBody)
+        //console.log(cardBody);
         HttpClient.request(urlCreatePaymentMethod,
           {
             method: "post",
             headers: { 'Authorization': 'Bearer ' + key },
             body: cardBody
           }
-        ).then(function (res) {
+        ).then((res) => {
           // get PaymentMethod ID to use in PaymentIntent/confirm
           var pm_id = JSON.parse(res.responseText).id;
-          console.log(res.responseText)
+          //console.log(res.responseText)
           //post request to confirm PaymentIntent
           let urlConfirmPaymentIntent = "https://api.stripe.com/v1/payment_intents/" + pi_id + "/confirm";
 
-          console.log("pm_id: " + pm_id);
+          //console.log("pm_id: " + pm_id);
           let confirmBody = new URLSearchParams();
           confirmBody.append('payment_method', pm_id);
           HttpClient.request(urlConfirmPaymentIntent,
@@ -86,28 +89,38 @@ const StripeConnection: ProcessorConnection<APIKeyCredentials, CardDetails> = {
               method: "post",
               headers: { 'Authorization': 'Bearer ' + key },
               body: confirmBody
-            }).then(function (res) {
-              console.log(JSON.parse(res.responseText))
-              let status = JSON.parse(res.responseText).status;
+            }).then((res) => {
+              let resJson = JSON.parse(res.responseText);
+              //console.log(resJson)
+              let status = resJson.status;
               console.log(status);
-              // let result = { processorTransactionId: "", transactionStatus: outcome }
-              // return new Promise<ParsedAuthorizationResponse>(() => result)
+              let statusMap = {
+                "requires_capture": "AUTHORISED",
+                "requires_payment_method": "DECLINED",
+                "processing": "SETTLING",
+
+              }
+              let result = { 
+                processorTransactionId: Math.floor(1000 + Math.random() * 9000),
+                transactionStatus: statusMap[status]||"FAILED",
+                errorMessage:"",
+                declineReason: ""
+              };
+              if (result.transactionStatus=="DECLINED") {
+                result.declineReason = status;
+              } else if (result.transactionStatus=="FAILED") {
+                result.errorMessage = resJson.error;
+              }
+              console.log(result);
+              resolve(result);
             });
 
         })
-      })
+      }).catch(err=> {
+        reject(new Error(err));
+      });
 
-
-
-
-    //   // confirm paymentIntent - /v1/payment_intents/:id/confirm
-    //   // create a paymentMethod
-
-
-
-    return new Promise<ParsedAuthorizationResponse>(() => console.log('return PAR'))
-
-
+    });
     //throw new Error('Method Not Implemented');
   },
 
